92e5cce991270484181bcbd4e7d4e87b354982d1
Ubuntu <ubuntu@ip-172-31-46-103.ec2.internal>
2019-06-25 22:58:13 +0000

    final adding send email script and connect on forms

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -63,6 +63,9 @@ function fetchGitHubInformation(event) {
                 if (errorResponse.status === 404){
                     $("#gh-user-data").html(
                         `<h2> No Info found for user ${username}</h2>`);
+                }else if (errorResponse.status == 403){
+                    var resetTime = new Date(errorResponse.getResponseHeader(`x-RateLimit-Reset`)*1000);
+                    $("#gh-user-data").html(`<h4>Too many requests,please wait until $(resetTime.toLocaltimeString()}</h4>`);
                 } else {
                     console.log(errorResponse);
                     $("#gh-user-data").html(
