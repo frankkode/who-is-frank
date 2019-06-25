0fdc6b50585ec343b0b7f544889e423f25f83a04
Ubuntu <ubuntu@ip-172-31-46-103.ec2.internal>
2019-06-25 19:27:06 +0000

    finilising github script and view

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -16,7 +16,30 @@ function userInformationHtml(user){
 }
 
 
+function repoInformationHtml(repos) {
+    if (repos.length == 0) {
+        return `<div class="clearfix repo-list">No repos!!</div>`;
+    }
+    
+    var listItemsHtml = repos.map(function(repo){
+        return `<li>
+        <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+        </li>`;
+    });
+    return `<div class="clearfix repo-list">
+    <p>
+    <strong>Repo List :</strong>
+    </p>
+    <ul>
+    ${listItemsHtml.join("\n")}
+    </ul>
+    </div>`;
+}
+
+
 function fetchGitHubInformation(event) {
+    $("#gh-user-data").html("");
+    $("#gh-repo-data").html("");
     var username = $("#gh-username").val();
     if (!username) {
         $("#gh-user-data").html(`<h2>please enter a Github username </h2>`);
@@ -28,11 +51,14 @@ function fetchGitHubInformation(event) {
         <img src= "assets/css/loader.gif" alt="loader...."/>
         </div>`);
         $.when(
-            $.getJSON(`https://api.github.com/users/${username}`)
+            $.getJSON(`https://api.github.com/users/${username}`),
+            $.getJSON(`https://api.github.com/users/${username}/repos`)
             ).then(
-                function(response){
-                var userData = response;
+                function(firstResponse, secondResponse){
+                var userData = firstResponse[0];
+                var repoData = secondResponse[0];
                 $("#gh-user-data").html(userInformationHtml(userData));
+                $("#gh-repo-data").html(repoInformationHtml(repoData));
             }, function(errorResponse){
                 if (errorResponse.status === 404){
                     $("#gh-user-data").html(
@@ -44,5 +70,7 @@ function fetchGitHubInformation(event) {
                 }
             });
 }
+
+$(document).ready(fetchGitHubInformation);
 
 
